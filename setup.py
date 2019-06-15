d2ed4c051c723fa8e81139f22de9dcb1860a8551
justinnbt <j.boylan-toomey.08@aberdeen.ac.uk>
2019-06-15 13:17:56 +0100

    Added install requirements

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/justinnbt/Jaccardupy',
     packages=['jaccardupy'],
+    install_requires=['numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
