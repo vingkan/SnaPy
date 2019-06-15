07f1b2e4f87e882b218f3ccfdde48014452ea798
justinnbt <j.boylan-toomey.08@aberdeen.ac.uk>
2019-06-15 12:54:26 +0100

    Updated URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='jaccardupy',
       version='0.1',
       description='Near Duplicate Text Detection Library.',
-      url='https://github.com/justinnbt/LSH',
+      url='https://github.com/justinnbt/Jaccardupy',
       license='MIT',
       author='Justin Boylan-Toomey',
       author_email='justin.boylan-toomey@outlook.com',
