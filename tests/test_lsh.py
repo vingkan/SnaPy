1fdbb1df9306f21bed7ea409381460251f5b117d
justinnbt <j.boylan-toomey.08@aberdeen.ac.uk>
2019-08-20 21:28:22 +0100

    Added additional lsh tests

--- tests/test_lsh.py
+++ tests/test_lsh.py
@@ -19,15 +19,26 @@ content = [
     'The Great Red Spot is large enough to accommodate Earth within its boundaries.'
 ]
 
-minhash = MinHash(content)
+minhash = MinHash(content, seed=3)
 
 
-def test_empty_lsh():
+def test_initialize_from_empty_lsh():
     lsh = LSH()
     assert lsh.no_of_bands is None
     assert lsh._buckets == defaultdict(list)
     assert lsh._i_bucket == defaultdict(list)
+    assert lsh.permutations is None
+    lsh.update(minhash, labels)
+    assert list(lsh._i_bucket) == labels
+    buckets = lsh._buckets
+    assert buckets[4466445138223010106] == [1, 8]
+    assert buckets[-3939654010681976230] == [1, 4, 8]
+    assert lsh.permutations == 100
+    assert lsh.no_of_bands == 50
 
 
-def 
-
+def test_lsh_errors():
+    with pytest.raises(ValueError):
+        LSH(content)
+    with pytest.raises(ValueError):
+        LSH(labels=labels)
